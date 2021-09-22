81bc393ff08ec933b63fed69a0bf05ec4fdbe504
anonymous10137211 <78032402+anonymous10137211@users.noreply.github.com>
2021-09-22 12:41:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const axios = require('axios');
+
+const client = new Discord.client();
+
+client.on('ready', => {
+  console.log('Logged in as ${client.user.tag}!');
+})
+
+client.on('message', async msg => {
+  switch (msg.content) {
+    case "all hail memegod":
+      msg.reply("All Hail.");
+      break;
+    case "!!meme":
+      msg.channel.send("Here is your meme, meme god hailer.");
+      const img = await getMeme();
+      msg.channel.send(img);
+      break;
+   }
+})
+
+client.login(process.env.CLIENT_TOKEN);
